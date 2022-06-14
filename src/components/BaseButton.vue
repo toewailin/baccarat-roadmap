7e3fc7d6bed3bc719b3c38369bd97314697d577c
Karma Blackshaw <irenesejah29@gmail.com>
2022-06-14 16:38:26 +0800

    fix: select none for btn text

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-4 py-2 font-semibold text-sm rounded"
+    class="px-4 py-2 font-semibold text-sm rounded select-none"
     @click="e => $emit('click', e)"
   >
     <slot></slot>
