320e0226ba5d71b2ad3066c87f8a32b328499c89
Karma Blackshaw <irenesejah29@gmail.com>
2022-06-14 15:52:27 +0800

    feat: register base components plugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// plugins
+import registerBaseComponents from './plugins/register-base-components'
+
 Vue.config.productionTip = false
+
+Vue.use(registerBaseComponents)
 
 new Vue({
   router,
